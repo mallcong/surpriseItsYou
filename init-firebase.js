555cceba5225724828ccd2525bf37d6e84ec9c66
YunChoi <yunhoho@gmail.com>
2021-05-18 06:23:52 -0400

    Add files via upload

---
+++ init-firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB9RHFYViHzuOH7uJORduwpMfdis8NvzcQ",
+    authDomain: "surprise-779e3.firebaseapp.com",
+    projectId: "surprise-779e3",
+    storageBucket: "surprise-779e3.appspot.com",
+    messagingSenderId: "107298468159",
+    appId: "1:107298468159:web:2b5dfb04783c3d910e238a",
+    measurementId: "G-KNLPTWC629"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
